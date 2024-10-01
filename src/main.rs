969c3684cdac5547e4e9b07afbab23f244d84c0b
Thorsten Hapke <thorsten.hapke@sap.com>
2024-10-01 12:43:43 +0200

    merge

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
-use std::env;
 use snafu::Snafu;
+use std::env;
+use std::path::PathBuf;
 
 mod args;
 
@@ -11,9 +11,9 @@ use graph::graph::GraphBuilder;
 #[derive(Debug, Snafu)]
 pub enum Error {
     #[snafu(display("Build tree error"))]
-    BuildError{source: filesystem::Error},
+    BuildError { source: filesystem::Error },
     #[snafu(display("File listing error"))]
-    FileListError{source: filesystem::Error},
+    FileListError { source: filesystem::Error },
 }
 
 fn list_files_recursively(root_dir: &PathBuf) -> Vec<PathBuf> {
@@ -23,22 +23,26 @@ fn list_files_recursively(root_dir: &PathBuf) -> Vec<PathBuf> {
 }
 
 fn main() {
-
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     // args
     let args: Vec<String> = env::args().collect();
     let app = args::parse_cli_arguments();
-    let matches = app.try_get_matches_from(args).unwrap_or_else(|e| {e.exit();});
-    let max_level =  matches.get_one::<usize>("max");
-    
+    let matches = app.try_get_matches_from(args).unwrap_or_else(|e| {
+        e.exit();
+    });
+    let max_level = matches.get_one::<usize>("max");
+
     // List of files with path
-    let root_dir = matches.get_one::<String>("path").expect("Argument \"Path\" required!");
+    let root_dir = matches
+        .get_one::<String>("path")
+        .expect("Argument \"Path\" required!");
     let files_list = list_files_recursively(&PathBuf::from(&root_dir));
     // let files_list_str = files_list.iter().filter_map(|p| p.to_str()).collect::<Vec<String>>().context(FileListSnafu)?;
     let files_list_str = files_list.iter().map(|p| p.display().to_string()).collect();
-    
-    let output = FileSystem::print_file_list(files_list_str, max_level, Some(&PathBuf::from(root_dir)));
+
+    let _output =
+        FileSystem::print_file_list(files_list_str, max_level, Some(&PathBuf::from(root_dir)));
     // println!("{}",output);
 
     let fs = FileSystem::from_local(&PathBuf::from(root_dir));
@@ -47,11 +51,6 @@ fn main() {
     //     println!("{}", f.path.display());
     // }
 
-
     println!("{fs}");
     // println!("{}", fs);
-    
-
-
 }
-
